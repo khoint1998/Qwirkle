6a2da66d8a7404f236440dd68cf790111f9a7f04
Khoi <s3678755@rmit.edu.vn>
2019-04-18 22:26:20 +1000

    Upload

---
+++ qwirkle.cpp
@@ -0,0 +1,16 @@
+
+#include "LinkedList.h"
+
+#include <iostream>
+
+#define EXIT_SUCCESS    0
+
+int main(void) {
+   LinkedList* list = new LinkedList();
+   delete list;
+
+   std::cout << "Welcome to Qwirkle!" << std::endl;
+   std::cout << "-------------------" << std::endl;
+
+   return EXIT_SUCCESS;
+}
